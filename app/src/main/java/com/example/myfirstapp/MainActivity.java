2a1db5798ab7d6f5edc6f1d592d29379b0bc1c19
twopercentbutterdish <jackwilsbach123@gmail.com>
2021-03-20 15:32:42 -0400

    Initial Commit

--- app/src/main/java/com/example/myfirstapp/MainActivity.java
+++ app/src/main/java/com/example/myfirstapp/MainActivity.java
@@ -10,6 +10,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        // Can you see me, Aidan???
+        // Can you see me, Aidan????
     }
 }

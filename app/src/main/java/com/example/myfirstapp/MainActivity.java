ee3138338901917472927e339b98053221a24df0
twopercentbutterdish <jackwilsbach123@gmail.com>
2021-03-20 15:53:58 -0400

    Initial Commit

--- app/src/main/java/com/example/myfirstapp/MainActivity.java
+++ app/src/main/java/com/example/myfirstapp/MainActivity.java
@@ -11,5 +11,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         // Can you see me, Aidan????
+        // Sent again.
     }
 }

20d2e33c67d471ee0e7eae8dfc506f3be6d83233
twopercentbutterdish <jackwilsbach123@gmail.com>
2021-03-22 20:43:16 -0400

    Fourth commit.

--- app/src/main/java/com/example/myfirstapp/Copy.java
+++ app/src/main/java/com/example/myfirstapp/Copy.java
@@ -1,27 +1,113 @@
 package com.example.myfirstapp;
 
-import java.time.Clock;
-import java.time.Instant;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 
 public class Copy {
     /**
-     *  Field representing copy's completeness: 1 = complete, 0 = incomplete.
+     *  Field representing copy's completeness.
      */
-    private int complete;
-    /**
-     *  Field tracking the time from copy's creation to purchase.
-     */
-    private Clock time;
+    private boolean complete;
     /**
      *  Time stamp of copy's creation.
      */
-    private Instant creation;
+    private final Calendar creation;
     /**
      *  Time stamp of copy's purchase.
      */
-    private Instant purchase;
+    private Calendar purchase;
     /**
      *  Amount of time copy spent as listed.
      */
     private String lifespan;
+    /**
+     *  Listing's price
+     */
+    private double price;
+    /**
+     * Constructor.
+     */
+    public Copy(double cost, boolean completeness){
+        this.price = cost;
+        this.complete = completeness;
+        this.creation = Calendar.getInstance();
+    }
+
+    //--------------------------------------------------------------------------------------------//
+    /*
+     * Methods.
+     */
+    //--------------------------------------------------------------------------------------------//
+
+    /**
+     * DOC
+     */
+    public boolean isComplete(){
+        return this.complete;
+    }
+    /**
+     * DOC
+     */
+    public void changeComplete(){
+        this.complete = !this.complete;
+    }
+    /**
+     * DOC
+     */
+    public void markAsSold(){
+        this.purchase = Calendar.getInstance();
+        this.lifespan = createLifeSpan(this.creation, this.purchase);
+    }
+    /**
+     * DOC
+     */
+    public void changePrice(double newPrice){
+        this.price = newPrice;
+    }
+    /**
+     * DOC
+     */
+    public double getPrice(){
+        return this.price;
+    }
+    /**
+     * DOC
+     */
+    public String getCreation() {
+        return getDate(this.creation);
+    }
+    /**
+     * DOC
+     */
+    public String getPurchase(){
+        return getDate(this.purchase);
+    }
+    /**
+     * DOC
+     */
+    public String getLifeSpan(){
+        return this.lifespan;
+    }
+
+    //--------------------------------------------------------------------------------------------//
+    /*
+     * Helper methods.
+     */
+    //--------------------------------------------------------------------------------------------//
+
+    /**
+     * DOC
+     */
+    public String createLifeSpan(Calendar start, Calendar finish){
+        return getDate(start) + " - " + getDate(finish);
+    }
+    /**
+     * DOC
+     */
+    public String getDate(Calendar time){
+        SimpleDateFormat formatter = new SimpleDateFormat("dd-MM-yyyy HH:mm:ss");
+        return formatter.format(time.getTime());
+    }
 }
+
+
--- app/src/main/java/com/example/myfirstapp/Listing.java
+++ app/src/main/java/com/example/myfirstapp/Listing.java
@@ -1,6 +1,6 @@
 package com.example.myfirstapp;
 
-import java.util.AbstractMap;
+import java.util.AbstractMap.SimpleEntry;
 import java.util.ArrayDeque;
 import java.util.Map;
 import java.util.Queue;
@@ -11,27 +11,67 @@ public class Listing {
      */
     private Map.Entry<String, Queue<Copy>> game;
     /**
-     *  Listing's price
-     */
-    private double price;
-    /**
-     *  Copies sold.
-     */
-    private int sold;
-    /**
      * Constructor.
      */
-    public Listing(String name, double cost){
-        price = cost;
-        Queue<Copy> copies = new ArrayDeque<>();
-        game = new AbstractMap.SimpleEntry(name, copies);
+    public Listing(String name) {
+        Queue<Copy> copies = new ArrayDeque<Copy>();
+        this.game = new SimpleEntry<>(name, copies);
     }
+
+    //--------------------------------------------------------------------------------------------//
     /*
      * Methods.
      */
-    // TO_DO
-    // Add copy
-    // Get quantity.
-    // Change price.
-    // Increment sold.
+    //--------------------------------------------------------------------------------------------//
+
+    /*
+     *
+     */
+    public void addCopy(Copy item){
+            Queue<Copy> temp = this.game.getValue();
+            temp.add(item);
+            this.game.setValue(temp);
+    }
+    /*
+     *
+     */
+    public Copy sellCopy(){
+            Queue<Copy> temp = this.game.getValue();
+            Copy removed = temp.remove();
+            while (!removed.isComplete()){
+                temp.add(removed);
+                removed = temp.remove();
+            }
+        this.game.setValue(temp);
+        return removed;
+    }
+    /*
+     *
+     */
+    public int getQuantity(){
+        Queue<Copy> temp = this.game.getValue();
+        int size = temp.size();
+        this.game.setValue(temp);
+        return size;
+    }
+    /*
+     *
+     */
+    public void changePrice(double newPrice){
+        Queue<Copy> temp = this.game.getValue();
+        for (int i = 0; i < temp.size(); i++){
+            Copy changed = temp.remove();
+            changed.changePrice(newPrice);
+            temp.add(changed);
+        }
+    }
+    /*
+     *
+     */
+    public void changeName(String newName) {
+        Queue<Copy> temp = this.game.getValue();
+        this.game = new SimpleEntry(newName, temp);
+    }
+
+
 }
